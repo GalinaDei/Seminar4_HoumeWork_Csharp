849e4f0658c88b84ab9ce700db6d17a7e5afcfa7
Galina <gal-dej@yandex.ru>
2022-10-13 10:41:11 +0300

    Tash has done

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -5,4 +5,17 @@
 ввод элеменетов через консоль (через запятую. использовать метод string.Split())
 */
 
-string.Split();
+/*int[] array = {1,2,3,4,5};
+string massiv = string.Join(",",array);
+Console.WriteLine ($"{massiv}");
+*/
+  
+Console.WriteLine("Введите 8 чисел, разделенные запятой  пробелом");
+string text = Console.ReadLine()??"";
+string[] array = text.Split(", ");
+Console.Write($"{text} -> [");
+for (int i=0; i<array.Length-1; i++)
+{
+   Console.Write($"{array[i]}, ");
+}
+Console.Write($"{array[array.Length-1]}]");
